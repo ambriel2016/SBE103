044104559f615479fdb0128eb0251537f90757e8
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-17 13:02:05 -0500

    SBE103

---
+++ src/main/java/com/cristian/demo/Sbe103Application.java
@@ -0,0 +1,13 @@
+package com.cristian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbe103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sbe103Application.class, args);
+    }
+
+}
